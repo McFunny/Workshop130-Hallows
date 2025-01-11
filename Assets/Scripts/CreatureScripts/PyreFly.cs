fcb82aa860aab21daea538ddfbb047222545770b
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-01-11 14:16:21 -0500

    Testing pathfinding

--- Assets/Scripts/CreatureScripts/PyreFly.cs
+++ Assets/Scripts/CreatureScripts/PyreFly.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PyreFly : CreatureBehaviorScript
 {
     // Start is called before the first frame update
     void Start()
