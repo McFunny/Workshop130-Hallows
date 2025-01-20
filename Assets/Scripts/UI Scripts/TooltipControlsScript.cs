fab10d9e60bb253aa106b71b49b45b9ba4ff19e7
SolidZan <zanman263@gmail.com>
2025-01-19 23:57:08 -0500

    Tooltip Controls Stuff

--- Assets/Scripts/UI Scripts/TooltipControlsScript.cs
+++ Assets/Scripts/UI Scripts/TooltipControlsScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class TooltipControlsScript : MonoBehaviour
 {
-    [SerializeField] private GameObject container;
-    public TextMeshProUGUI[] containerArray;
+    [SerializeField] private GameObject container, defaultContainer;
+    public GameObject[] containerArray;
     [SerializeField] private TextMeshProUGUI textBox;
 
+    void Start()
+    {
+        DefaultControls();
+    }
     public void SelectedItem()
     {  
         if (HotbarDisplay.currentSlot.AssignedInventorySlot != null && HotbarDisplay.currentSlot.AssignedInventorySlot.ItemData != null)
@@ -24,6 +28,7 @@ public class TooltipControlsScript : MonoBehaviour
                     for(int i = 0; i < t_item.itemInputsKBM.Count; i++)
                     {
                         var newObj = Instantiate(textBox, container.transform, worldPositionStays:false);
+                        newObj.tag = "Tip";
                         newObj.text = t_item.itemInputsKBM[i];
                     }
                 }
@@ -41,6 +46,7 @@ public class TooltipControlsScript : MonoBehaviour
                     for (int i = 0; i < o_item.AssignedInventorySlot.ItemData.itemInputsKBM.Count; i++)
                     {
                         var newObj = Instantiate(textBox, container.transform, worldPositionStays:false);
+                        newObj.tag = "Tip";
                         newObj.text = o_item.AssignedInventorySlot.ItemData.itemInputsKBM[i];
                     }
                 }
@@ -59,10 +65,25 @@ public class TooltipControlsScript : MonoBehaviour
 
     public void DestoryTextObjects()
     {
-        containerArray = GetComponentsInChildren<TextMeshProUGUI>();
+        containerArray = GameObject.FindGameObjectsWithTag("Tip");//GetComponentsInChildren<TextMeshProUGUI>();
         for(int i = 0; i < containerArray.Length; i++)
         {
             Destroy(containerArray[i].gameObject);
         }
+    }
+
+    public void DefaultControls()
+    {
+        var invObj = Instantiate(textBox, defaultContainer.transform, worldPositionStays:false);
+        invObj.text = "E - Open Bag";
+
+        var lmbObj = Instantiate(textBox, defaultContainer.transform, worldPositionStays:false);
+        lmbObj.text = "LMB - Use Item";
+
+        var rmbObj = Instantiate(textBox, defaultContainer.transform, worldPositionStays:false);
+        rmbObj.text = "RMB - Use Item on Structure";
+
+        var spaceObj = Instantiate(textBox, defaultContainer.transform, worldPositionStays:false);
+        spaceObj.text = "Space - Interact";
     }
 }
