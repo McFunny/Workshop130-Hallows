f5f75e2e6c2da389b3b034833cea47e8e92c78bb
Abner-Wilhelm <abnerwilhelm@gmail.com>
2025-01-20 17:34:19 -0500

    Puzzle Completed

---
+++ Assets/BrazierPuzzleManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BrazierPuzzleManager : MonoBehaviour
+{
+    [SerializeField] private List<PuzzleBrazier> brazierList = new List<PuzzleBrazier>();
+
+    private void Start()
+    {
+        InitializePuzzles();
+    }
+
+    private void InitializePuzzles()
+    {
+        foreach (var brazier in brazierList)
+        {
+            brazier.OnInteractionComplete += (completedPillar) => CheckPuzzleCompletion();
+        }
+    }
+
+    private void CheckPuzzleCompletion()
+    {
+        int puzzlesCorrect = 0;
+        foreach (var brazier in brazierList)
+        {
+            if (brazier.correctFire == brazier.currentFire)
+            {
+                puzzlesCorrect++;
+                if (puzzlesCorrect == brazierList.Count)
+                {
+                    LockPuzzle();
+                }
+            }
+        }
+    }
+
+    private void LockPuzzle()
+    {
+        Debug.Log("Brazier Puzzle Completed");
+        foreach (var brazier in brazierList)
+        {
+            brazier.isLocked = true;
+        }
+    }
+}
