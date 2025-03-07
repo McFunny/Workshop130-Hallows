89ab713b0c94d9d3179598394c4f9e1095d2c146
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-06 21:26:59 -0500

    Tutorial gibs

--- Assets/Scripts/Misc/Tutorial.cs
+++ Assets/Scripts/Misc/Tutorial.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
+    public PopupScript tillP, plantP, waterP, killP, weedP, completeP;
+    
     // Start is called before the first frame update
     void Start()
     {
