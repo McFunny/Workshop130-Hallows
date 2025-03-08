6bcd8ba489b35d2cfac3b5357e95a5644d0ebb03
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-08 13:23:08 -0500

    mistbreaker

--- Assets/Scripts/CreatureScripts/MistWalker.cs
+++ Assets/Scripts/CreatureScripts/MistWalker.cs
@@ -481,7 +481,7 @@ public class MistWalker : CreatureBehaviorScript
             StartCoroutine(SwipePlayer());
             transform.LookAt(player.position);
         }
-        else if (distance > attackRange && distance <= lungeRange && canLunge && !PlayerInteraction.Instance.torchLit)
+        else if (distance > attackRange && distance <= lungeRange && canLunge && !PlayerInteraction.Instance.torchLit && HandItemManager.Instance.GetCurrentType() == ToolType.Torch)
         {
             StartCoroutine(LungeAtPlayer());
         }
