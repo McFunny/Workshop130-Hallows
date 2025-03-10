496b4c4a8368f8e67a38cf989bb82173e246235d
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-10 14:24:09 -0400

    Lauren stuff

--- Assets/Scripts/Wilderness/WildernessMap.cs
+++ Assets/Scripts/Wilderness/WildernessMap.cs
@@ -15,6 +15,8 @@ public class WildernessMap : MonoBehaviour
 
     public GameObject forageablePrefab;//to make sure it no spawn new one
 
+    List<GameObject> currentInteractables = new List<GameObject>();
+
     void Start()
     {
         if(!WildernessManager.Instance.allMaps.Contains(this))
@@ -57,13 +59,15 @@ public class WildernessMap : MonoBehaviour
                 }
                 if(prefab != null)
                 {
+                    GameObject newPrefab;
                     if(prefab == forageablePrefab)
                     {
-                        GameObject newPrefab = StructurePoolManager.Instance.GrabForageable(true);
+                        newPrefab = StructurePoolManager.Instance.GrabForageable(true);
                         newPrefab.transform.position = interactablePositions[r].position;
                     }
-                    else Instantiate(prefab, interactablePositions[r].position, Quaternion.identity);
+                    else newPrefab = Instantiate(prefab, interactablePositions[r].position, Quaternion.identity);
                     usedSpots.Add(interactablePositions[r]);
+                    currentInteractables.Add(newPrefab);
                 }
             }
         }
@@ -86,5 +90,14 @@ public class WildernessMap : MonoBehaviour
         {
             obstacles[i].SetActive(false);
         }
+
+        foreach (GameObject obj in currentInteractables)
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+            }
+        }
+        currentInteractables.Clear();
     }
 }
