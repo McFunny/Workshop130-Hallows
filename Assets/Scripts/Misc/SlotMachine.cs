5c9c5b069705a6fb8e230a0291d820b948767349
Abner-Wilhelm <abnerwilhelm@gmail.com>
2025-03-14 22:12:51 -0400

    Frothing at the mouth rn I love gambling so much!!!!!!!!

--- Assets/Scripts/Misc/SlotMachine.cs
+++ Assets/Scripts/Misc/SlotMachine.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class SlotMachine : MonoBehaviour
+public class SlotMachine : MonoBehaviour,IInteractable
 {
     [SerializeField] GameObject Slot1;
     [SerializeField] GameObject Slot2;
@@ -12,9 +15,33 @@ public class SlotMachine : MonoBehaviour
     public int SlotIndex2;
     public int SlotIndex3;
 
-    public float speed;
+    public int moneySpent = 0;
+    public int cost = 50;
 
-    public bool coroutineRunning = false;
+    public float speed, timePerSlot;
+
+    public InventoryItemData bullet;
+
+    public List<InventoryItemData> randomPlant = new List<InventoryItemData>();
+    public List<InventoryItemData> bannedCrops = new List<InventoryItemData>();
+    public InventoryItemData carrotSeed;
+
+    public Transform itemCollection;
+
+    public GameObject droppedItem;
+
+    private GameObject pyreflyEnemy;
+    public GameObject pyreflyPrefab;
+
+    public bool coroutineRunning = false, broken = false, puzzleSolved = false;
+
+    public List<GameObject> highlight = new List<GameObject>();
+    List<Material> highlightMaterial = new List<Material>();
+    bool highlightEnabled;
+
+
+
+    public UnityAction<IInteractable> OnInteractionComplete { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
 
     void Start()
     {
@@ -24,39 +51,121 @@ public class SlotMachine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.G) && !coroutineRunning)
+      if (Input.GetKeyUp(KeyCode.G) && !coroutineRunning)
         {
-            StartCoroutine(LetsGamble());
-        }    
+            if (!broken)
+            {
+                if (PlayerInteraction.Instance.currentMoney >= cost && !coroutineRunning)
+                {
+                    PlayerInteraction.Instance.currentMoney -= cost;
+                    moneySpent += cost;
+                    StartCoroutine(LetsGamble());
+                }
+            }
+        }
     }
 
     IEnumerator LetsGamble()
     {
         coroutineRunning = true;
-        int r = Random.Range(0,2);
-        SlotIndex1 = Random.Range(1, 7);
-        if (r == 1) { SlotIndex2 = Random.Range(1, 7);}
-        else if (r == 0) { SlotIndex2 = SlotIndex1;}
-        r = Random.Range(0, 2);
-        if (r == 0 && SlotIndex1 == SlotIndex2) { SlotIndex3 = SlotIndex2; }
-        else if (r == 1) { SlotIndex3 = Random.Range(1, 7); }
+        if (!puzzleSolved && moneySpent >= 500)
+        {
+            SlotIndex1 = 1;
+            SlotIndex2 = 1;
+            SlotIndex3 = 1;
+            yield return StartCoroutine(RotateSlots());
+            Reward();
+            coroutineRunning = false;
+        }
+        else
+        {
+            int r = Random.Range(0, 2);
+            SlotIndex1 = Random.Range(1, 7);
+            if (r == 1) { SlotIndex2 = Random.Range(1, 7); }
+            else if (r == 0) { SlotIndex2 = SlotIndex1; }
+            r = Random.Range(0, 2);
+            if (r == 0 && SlotIndex1 == SlotIndex2) { SlotIndex3 = SlotIndex2; }
+            else if (r == 1) { SlotIndex3 = Random.Range(1, 7); }
 
-        yield return StartCoroutine(RotateSlots());
+            yield return StartCoroutine(RotateSlots());
 
-        yield return new WaitForSeconds(r);
-        coroutineRunning = false;
+            yield return new WaitForSeconds(r);
+
+            Reward();
+            coroutineRunning = false;
+        }
     }
 
-    IEnumerator RotateSlots()
+    private void Reward()
     {
-        StartCoroutine(SpinSlot(Slot1.transform, 2f)); 
-        StartCoroutine(SpinSlot(Slot2.transform, 4f)); 
-        StartCoroutine(SpinSlot(Slot3.transform, 6f));
-        yield return new WaitForSeconds(2f);
+        if (SlotIndex1 == SlotIndex2 && SlotIndex2 == SlotIndex3)
+        {
+            switch (SlotIndex1)
+            {
+                case 1:
+                    if (!puzzleSolved)
+                    {
+                        puzzleSolved = true;
+                        //play sound effect
+                    }
+                    else PlayerInteraction.Instance.currentMoney += (cost * 4);
+                    break;
+                case 2:
+
+                    PlayerInteraction.Instance.currentMoney += (cost * 2);
+                    break;
+                case 3:
+                    
+                    droppedItem = ItemPoolManager.Instance.GrabItem(bullet);
+                    droppedItem.transform.position = itemCollection.position;
+                    break;
+                case 4:
+                    if (puzzleSolved)
+                    {
+                        StartCoroutine(SummonPyreFly());
+                        broken = true;
+                    }
+                    StartCoroutine(SummonPyreFly());
+                    //play broken sound effect
+                    break;
+                case 5:
+                    List<InventoryItemData> randomPlant = Database.Instance.GetAllCrops().Cast<InventoryItemData>().ToList();
+                    int r = Random.Range(1, randomPlant.Count);
+                    for (int i = 0; i < bannedCrops.Count; i++)
+                        {
+                        if (randomPlant[r] == bannedCrops[i])
+                        {
+                            randomPlant[r] = carrotSeed;
+                        }
+                        }
+                    droppedItem = ItemPoolManager.Instance.GrabItem(randomPlant[r]);
+                    droppedItem.transform.position = itemCollection.position;
+                    break;
+                case 6:
+                    StartCoroutine(SummonPyreFly());
+                    break;
+            }
+        }
+    }
+
+    IEnumerator SummonPyreFly()
+    {
+        pyreflyEnemy = Instantiate(pyreflyPrefab, itemCollection.position, itemCollection.rotation);
+        yield return new WaitForSeconds(0.5f);
+        PyreFly pyreFlyScript = pyreflyEnemy.GetComponent<PyreFly>();
+        pyreFlyScript.OnDestroy();
+    }
+
+        IEnumerator RotateSlots()
+    {
+        StartCoroutine(SpinSlot(Slot1.transform, timePerSlot)); 
+        StartCoroutine(SpinSlot(Slot2.transform, timePerSlot*2)); 
+        StartCoroutine(SpinSlot(Slot3.transform, timePerSlot*3));
+        yield return new WaitForSeconds(timePerSlot);
         Slot1.transform.rotation = Quaternion.Euler(0, 0, SlotIndex1 * 60f);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(timePerSlot);
         Slot2.transform.rotation = Quaternion.Euler(0, 0, SlotIndex2 * 60f);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(timePerSlot);
         Slot3.transform.rotation = Quaternion.Euler(0, 0, SlotIndex3 * 60f);
     }
 
@@ -68,6 +177,75 @@ public class SlotMachine : MonoBehaviour
             slot.rotation = Quaternion.Lerp(slot.rotation, Quaternion.Euler(slot.eulerAngles.x, slot.eulerAngles.y, slot.eulerAngles.z + 30f), Time.deltaTime * speed);
             elapsedTime += Time.deltaTime;
             yield return null;
+        }
+    }
+
+    public void Interact(PlayerInteraction interactor, out bool interactSuccessful)
+    {
+        interactSuccessful = false;
+        if (!broken)
+        {
+            if (PlayerInteraction.Instance.currentMoney >= cost && !coroutineRunning)
+            {
+                PlayerInteraction.Instance.currentMoney -= cost;
+                moneySpent += cost;
+                StartCoroutine(LetsGamble());
+                interactSuccessful = true;
+            }
+        }
+    }
+
+    public void InteractWithItem(PlayerInteraction interactor, out bool interactSuccessful, InventoryItemData item)
+    {
+        interactSuccessful = false;
+    }
+
+    public void EndInteraction()
+    {
+       
+    }
+
+    public void ToggleHighlight(bool enable)
+    {
+        if (highlight.Count == 0) return;
+        if (highlightMaterial.Count == 0)
+        {
+            foreach (GameObject thing in highlight)
+                highlightMaterial.Add(highlight[0].GetComponentInChildren<MeshRenderer>().material);
+        }
+        if (enable && !highlightEnabled)
+        {
+            highlightEnabled = true;
+            foreach (GameObject thing in highlight) thing.SetActive(true);
+            StartCoroutine(HightlightFlash());
+        }
+
+        if (!enable && highlightEnabled)
+        {
+            highlightEnabled = false;
+            foreach (GameObject thing in highlight) thing.SetActive(false);
+        }
+    }
+
+    IEnumerator HightlightFlash()
+    {
+        float power = 1;
+        while (highlightEnabled)
+        {
+            do
+            {
+                yield return new WaitForSeconds(0.1f);
+                power -= 0.05f;
+                foreach (Material mat in highlightMaterial) mat.SetFloat("_Fresnel_Power", power);
+            }
+            while (power > 0.7f && highlightEnabled);
+            do
+            {
+                yield return new WaitForSeconds(0.1f);
+                power += 0.05f;
+                foreach (Material mat in highlightMaterial) mat.SetFloat("_Fresnel_Power", power);
+            }
+            while (power < 1.9f && highlightEnabled);
         }
     }
 }
