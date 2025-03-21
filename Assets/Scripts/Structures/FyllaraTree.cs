7e92f5058d1164f2e3dc1c2f4f446e5f73eb137f
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-21 13:33:52 -0400

    Nut

--- Assets/Scripts/Structures/FyllaraTree.cs
+++ Assets/Scripts/Structures/FyllaraTree.cs
@@ -6,13 +6,19 @@ public class FyllaraTree : StructureBehaviorScript
 {
     public GameObject treeNut;
 
-    public Transform[] nutSpawns;
-    public SpriteRenderer renderer;
+    public List<Transform> nutSpawns; //possible places nuts can spawn
+    List<Transform> nutSpawnsInUse; //used spots
+    public SpriteRenderer renderer; //for water
     bool isFilled = false;
     public int treeStage = -1;
 
-    public GameObject[] treeStages;
-    public GameObject[] treeNuts;
+    public GameObject[] treeStages; //Tree objects
+    public List<GameObject> currentTreeNuts; //Current nuts on the tree
+
+    int progressUntilNextGrowth = 0;
+    int maxProgress = 2;
+
+    //When nuts drop, have the item fling to the player
 
 
     void Awake()
@@ -23,8 +29,12 @@ public class FyllaraTree : StructureBehaviorScript
     void Start()
     {
         base.Start();
-        //if(treeStage == -1) RandomizeTreeStage();
-        //OnDamage += TreeNutDrop;
+        if(treeStage == -1) RandomizeTreeStage();
+        OnDamage += TreeNutDrop;
+
+        renderer.enabled = false;
+
+        transform.localEulerAngles = new Vector3(0, Random.Range(0,360), 0);
     }
 
     void Update()
@@ -43,19 +53,75 @@ public class FyllaraTree : StructureBehaviorScript
         success = false;
         if(type == ToolType.WateringCan && PlayerInteraction.Instance.waterHeld > 0 && !isFilled)
         {
-            //FillWithWater();
+            FillWithWater();
             success = true;
         }
     }
 
+    public override void HitWithWater()
+    {
+        if(!isFilled) FillWithWater();
+    }
+
     public override void HourPassed()
     {
-        //
+        if(isFilled && treeStage == treeStages.Length)
+        {
+            progressUntilNextGrowth += Random.Range(1,3);
+            if(progressUntilNextGrowth < maxProgress) return;
+            progressUntilNextGrowth = 0;
+            isFilled = false;
+            renderer.enabled = false;
+            PopulateTreeNut();
+        }
+    }
+
+    void FillWithWater()
+    {
+        isFilled = true;
+        renderer.enabled = true;
+    }
+
+    void TreeNutDrop()
+    {
+        for(int i = 0; i < currentTreeNuts.Count; i++)
+        {
+            if(currentTreeNuts[i])
+            {
+                currentTreeNuts[i].GetComponent<Rigidbody>().useGravity = true;
+                return;
+            }
+        }
+    }
+
+    void PopulateTreeNut()
+    {
+        for(int i = 0; i < nutSpawns.Count; i++)
+        {
+            if(nutSpawnsInUse[i] != nutSpawns[i])
+            {
+                nutSpawnsInUse.Add(nutSpawns[i]);
+                currentTreeNuts.Add(Instantiate(treeNut, nutSpawns[i].position, Quaternion.identity));
+                return;
+            }
+        }
+    }
+
+    void RandomizeTreeStage()
+    {
+        //Right now just spawn it as a big tree
+        treeStage = treeStages.Length - 1;
+
+        for(int i = 0; i < treeStages.Length; i++)
+        {
+            if(i == treeStage) treeStages[i].SetActive(true);
+            else treeStages[i].SetActive(false);
+        }
     }
 
     void OnDestroy()
     {
-        //OnDamage -= TreeNutDrop;
+        OnDamage -= TreeNutDrop;
         base.OnDestroy();
     }
 }
