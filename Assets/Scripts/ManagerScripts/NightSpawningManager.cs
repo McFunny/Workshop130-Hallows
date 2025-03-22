691682238c751a5d8752bf1fabb2d87715090f93
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-22 15:50:10 -0400

    Amanda? yea

--- Assets/Scripts/ManagerScripts/NightSpawningManager.cs
+++ Assets/Scripts/ManagerScripts/NightSpawningManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NightSpawningManager : MonoBehaviour
 {
@@ -397,6 +398,7 @@ public class NightSpawningManager : MonoBehaviour
         //AmbientAudioManager.Instance.FadeMusic();
         yield return new WaitForSeconds(3);
         //Credits screen
+        SceneManager.LoadSceneAsync(2);
     }
 
 
