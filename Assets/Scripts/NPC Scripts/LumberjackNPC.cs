08f82f289678b0216f0e2309552cac1d5e3b3abe
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-21 22:04:17 -0400

    Hi Abner

--- Assets/Scripts/NPC Scripts/LumberjackNPC.cs
+++ Assets/Scripts/NPC Scripts/LumberjackNPC.cs
@@ -14,14 +14,6 @@ public class LumberjackNPC : NPC, ITalkable
 
     public Quest treeQuest;
 
-    [Header("Nut Stuff")]
-    public Transform nutFocalPoint;
-    public Animator nutMachineAnim;
-    public GameObject nut;
-    public Animator nutAnim;
-    public InventoryItemData[] possibleNutItems;
-    public float[] nutItemWeight;
-
     protected override void Awake() //Awake in NPC.cs assigns the dialoguecontroller
     {
         base.Awake();
@@ -136,22 +128,8 @@ public class LumberjackNPC : NPC, ITalkable
 
         else if(item == treeNut)
         {
-            if(movementHandler.isWorking) //I am going to chop ur nuts
-            {
-                //nut stuff
-                //currentPath = 3;
-                //currentType = PathType.ItemSpecific;
-                HotbarDisplay.currentSlot.AssignedInventorySlot.RemoveFromStack(1);
-                PlayerInventoryHolder.Instance.UpdateInventory();
-                StartCoroutine(ChopNut());
-                interactSuccessful = true;
-                return;
-            }
-            else //Come see me at my place so I can chop ur nuts
-            {
-                currentPath = 2;
-                currentType = PathType.ItemSpecific;
-            }
+            currentPath = 2;
+            currentType = PathType.ItemSpecific;
         }
 
         else if(item.staminaValue > 0)
@@ -291,32 +269,6 @@ public class LumberjackNPC : NPC, ITalkable
     {
         if(GameSaveData.Instance.lumber_offersDeal) return true;
         return false;
-    }
-
-    IEnumerator ChopNut()
-    {
-        //put player focal point on the machine, do the machine anim stuff, spawn item, then break focal point
-        PlayerMovement.restrictMovementTokens++;
-        PlayerCam.Instance.NewObjectOfInterest(nutFocalPoint.position);
-        yield return new WaitForSeconds(1.5f);
-        GameObject droppedItem = ItemPoolManager.Instance.GrabItem(RandomNutItem());
-        droppedItem.transform.position = new Vector3(nutFocalPoint.position.x, nutFocalPoint.position.y + 0.5f, nutFocalPoint.position.z);
-        yield return new WaitForSeconds(1.5f);
-        PlayerCam.Instance.ClearObjectOfInterest();
-        PlayerMovement.restrictMovementTokens--;
-    }
-
-    InventoryItemData RandomNutItem()
-    {
-        int x = 0;
-        while(x < 10)
-        {
-            int i = Random.Range(0, possibleNutItems.Length);
-            float r = Random.Range(0f,1f);
-            if(r < nutItemWeight[i]) return possibleNutItems[i];
-            x++;
-        }
-        return possibleNutItems[0];
     }
 
 }
---
+++ Assets/Scripts/Structures/Interactables/NutMachine.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NutMachine : MonoBehaviour, IInteractable
+{
+    public UnityAction<IInteractable> OnInteractionComplete { get; set; }
+
+    bool canBeUsed = true;
+    public AudioSource source;
+    public AudioClip nutCut;
+
+    public List<GameObject> highlight = new List<GameObject>();
+    List<Material> highlightMaterial = new List<Material>();
+    bool highlightEnabled;
+
+    [Header("Nut Stuff")]
+    public Transform nutSpawn;
+    public Animator nutMachineAnim;
+    public InventoryItemData[] possibleNutItems;
+    public float[] nutItemWeight;
+    public InventoryItemData treeNut;
+
+    Vector3 lNutPos, rNutPos;
+    Quaternion lNutRot, rNutRot;
+    public GameObject lNut, rNut;
+    public Rigidbody lRB, rRB;
+
+    public GameObject lNutPrefab, rNutPrefab;
+    public ParticleSystem abnerParticles;
+
+    public void Interact(PlayerInteraction interactor, out bool interactSuccessful)
+    {
+        interactSuccessful = true;
+    }
+
+    public void InteractWithItem(PlayerInteraction interactor, out bool interactSuccessful, InventoryItemData item)
+    {
+        if(item == treeNut && canBeUsed)
+        {
+            HotbarDisplay.currentSlot.AssignedInventorySlot.RemoveFromStack(1);
+            PlayerInventoryHolder.Instance.UpdateInventory();
+            StartCoroutine(ChopNut());
+            interactSuccessful = true;
+            return;
+        }
+        interactSuccessful = false;
+    }
+    
+    public void EndInteraction()
+    {
+       
+    }
+
+    public void Start()
+    {
+        lNutRot = lNut.transform.rotation;
+        rNutRot = rNut.transform.rotation;
+
+        lNutPos = lNut.transform.position;
+        rNutPos = lNut.transform.position;
+
+        lNut.SetActive(false);
+        rNut.SetActive(false);
+    }
+
+    public void Update()
+    {
+        if(!canBeUsed && highlightEnabled) ToggleHighlight(false);
+    }
+
+    IEnumerator ChopNut()
+    {
+        //put player focal point on the machine, do the machine anim stuff, spawn item, then break focal point
+        canBeUsed = false;
+        if(!lNut || !rNut) InstantiateNuts();
+        else
+        {
+            lNut.SetActive(true);
+            rNut.SetActive(true);
+        }
+
+        nutMachineAnim.Play("nutcracker");
+        yield return new WaitForSeconds(1.1f);
+        GameObject droppedItem = ItemPoolManager.Instance.GrabItem(RandomNutItem());
+        Rigidbody itemRB = droppedItem.GetComponent<Rigidbody>();
+        itemRB = droppedItem.GetComponent<Rigidbody>();
+        itemRB.AddForce(Vector3.forward * 20);
+        itemRB.AddForce(Vector3.up * 10);
+
+        lRB.isKinematic = false;
+        rRB.isKinematic = false;
+        source.PlayOneShot(nutCut);
+        droppedItem.transform.position = new Vector3(nutSpawn.position.x, nutSpawn.position.y, nutSpawn.position.z);
+        abnerParticles.Play();
+        yield return new WaitForSeconds(0.3f);
+        lNut = null;
+        rNut = null;
+        //lNut.SetActive(false);
+        //rNut.SetActive(false);
+        //ResetNutPosition();
+        canBeUsed = true;
+    }
+
+    InventoryItemData RandomNutItem()
+    {
+        int x = 0;
+        while(x < 10)
+        {
+            int i = Random.Range(0, possibleNutItems.Length);
+            float r = Random.Range(0f,1f);
+            if(r < nutItemWeight[i]) return possibleNutItems[i];
+            x++;
+        }
+        return possibleNutItems[0];
+    }
+
+    void ResetNutPosition()
+    {
+        lRB.isKinematic = true;
+        rRB.isKinematic = true;
+
+        lNut.transform.position = lNutPos;
+        rNut.transform.position = rNutPos;
+
+        lNut.transform.rotation = lNutRot;
+        rNut.transform.rotation = rNutRot;
+    }
+
+    void InstantiateNuts()
+    {
+        lNut = Instantiate(lNutPrefab, lNutPos, lNutRot);
+        rNut = Instantiate(rNutPrefab, rNutPos, rNutRot);
+        lRB = lNut.GetComponent<Rigidbody>();
+        rRB = rNut.GetComponent<Rigidbody>();
+    }
+
+
+
+    public void ToggleHighlight(bool enable)
+    {
+        if(highlight.Count == 0) return;
+        if(highlightMaterial.Count == 0)
+        {
+            foreach(GameObject thing in highlight) highlightMaterial.Add(highlight[0].GetComponentInChildren<MeshRenderer>().material);
+        }
+        if(enable && !highlightEnabled && canBeUsed)
+        {
+            highlightEnabled = true;
+            foreach(GameObject thing in highlight) thing.SetActive(true);
+            StartCoroutine(HightlightFlash());
+        }
+
+        if(!enable && highlightEnabled)
+        {
+            highlightEnabled = false;
+            foreach(GameObject thing in highlight) thing.SetActive(false);
+        }
+    }
+
+    IEnumerator HightlightFlash()
+    {
+        float power = 1;
+        while(highlightEnabled)
+        {
+            do
+            {
+                yield return new WaitForSeconds(0.1f);
+                power -= 0.05f;
+                foreach(Material mat in highlightMaterial) mat.SetFloat("_Fresnel_Power", power);
+            }
+            while(power > 0.7f && highlightEnabled);
+            do
+            {
+                yield return new WaitForSeconds(0.1f);
+                power += 0.05f;
+                foreach(Material mat in highlightMaterial) mat.SetFloat("_Fresnel_Power", power);
+            }
+            while(power < 1.9f && highlightEnabled);
+        }
+    }
+}
