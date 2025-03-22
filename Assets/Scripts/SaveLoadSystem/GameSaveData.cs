ce67c5076e938e80ba7b6d52c2779f0f12e59495
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-03-22 13:54:02 -0400

    Quest Fix

--- Assets/Scripts/SaveLoadSystem/GameSaveData.cs
+++ Assets/Scripts/SaveLoadSystem/GameSaveData.cs
@@ -87,7 +87,8 @@ public class GameSaveData : MonoBehaviour
             PlayerInteraction.Instance.totalMoneyEarned = data.allGameSaveData.pTotalMoneyEarned;
             TimeManager.Instance.dayNum = data.allGameSaveData.pDayNumber;
 
-            for(int i = 0; i < data.allGameSaveData.activeQuests.Length; i++) QuestManager.Instance.activeQuests.Add(data.allGameSaveData.activeQuests[i]);
+            //for(int i = 0; i < data.allGameSaveData.activeQuests.Length; i++) QuestManager.Instance.activeQuests.Add(data.allGameSaveData.activeQuests[i]);
+            QuestManager.Instance.LoadData(data.allGameSaveData);
 
             tutorialMerchantSpoke = data.allGameSaveData.tutorialMerchantSpoke;
             rascalWantsFood = data.allGameSaveData.rascalWantsFood;
@@ -122,6 +123,9 @@ public class GameSaveData : MonoBehaviour
         public int pDayNumber;
 
         public Quest[] activeQuests;
+        public FetchQuest[] activeFetchQuests;
+        public HuntQuest[] activeHuntQuests;
+        public GrowQuest[] activeGrowQuests;
 
         public bool tutorialMerchantSpoke;
         public bool rascalWantsFood;
@@ -153,7 +157,9 @@ public class GameSaveData : MonoBehaviour
             pTotalMoneyEarned = PlayerInteraction.Instance.totalMoneyEarned;
             pDayNumber = TimeManager.Instance.dayNum;
 
-            activeQuests = QuestManager.Instance.activeQuests.ToArray();
+            //activeQuests = QuestManager.Instance.activeQuests.ToArray();
+
+            QuestManager.Instance.SaveQuestData(out activeQuests, out activeFetchQuests, out activeHuntQuests, out activeGrowQuests);
 
 
             tutorialMerchantSpoke = data.tutorialMerchantSpoke;
