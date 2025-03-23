2b2a5d45816b2c3877ae7a31e86f752c7fac9e6b
SolidZan <zanman263@gmail.com>
2025-03-23 02:03:10 -0400

    Basic Loading Screen

--- Assets/Scripts/UI Scripts/MainMenuScript.cs
+++ Assets/Scripts/UI Scripts/MainMenuScript.cs
@@ -41,6 +41,8 @@ public class MainMenuScript : MonoBehaviour
     public List<FileData> fileDatas = new List<FileData>();
     public static int currentSaveSlot = -1;//-1 means nothing is selected
     public bool isNewGame;
+    
+    public GameObject loadingScreen;
 
     // Start is called before the first frame update
     void Awake()
@@ -245,7 +247,28 @@ public class MainMenuScript : MonoBehaviour
     {
         FadeScreen.coverScreen = true;
         yield return new WaitForSecondsRealtime(2);
-        SceneManager.LoadSceneAsync(1);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1);
+        loadingScreen.SetActive(true);
+        var loadText = loadingScreen.GetComponentInChildren<TextMeshProUGUI>();
+
+        var load1 = "Loading";
+        var load2 = "Loading.";
+        var load3 = "Loading..";
+        var load4 = "Loading...";
+
+        while(!operation.isDone)
+        {
+            loadText.text = load1;
+            yield return new WaitForSeconds(.2f);
+            loadText.text = load2;
+            yield return new WaitForSeconds(.2f);
+            loadText.text = load3;
+            yield return new WaitForSeconds(.2f);
+            loadText.text = load4;
+            yield return new WaitForSeconds(.2f);
+        }
+        //SceneManager.LoadSceneAsync(1);
     }
 
     public void OpenSettingsScreen()
