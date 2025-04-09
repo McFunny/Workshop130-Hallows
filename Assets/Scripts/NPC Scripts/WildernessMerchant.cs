523af0a24d9f188655229b730c9ff8eb0d652024
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-04-09 18:09:57 -0400

    stinger

--- Assets/Scripts/NPC Scripts/WildernessMerchant.cs
+++ Assets/Scripts/NPC Scripts/WildernessMerchant.cs
@@ -9,6 +9,8 @@ public class WildernessMerchant : NPC, ITalkable
     public Transform merchantWagonPos;
     public Transform merchant;
 
+    public AudioSource source;
+
     void Start()
     {
         WildernessManager.Instance.wagon = this;
@@ -65,6 +67,7 @@ public class WildernessMerchant : NPC, ITalkable
 
         Vector3 pos = PlayerInteraction.Instance.transform.position + PlayerInteraction.Instance.mainCam.transform.forward * -5;
         pos.y = PlayerInteraction.Instance.transform.position.y;
+        source.Play();
         //pos = new Vector3(pos.x, pos.y - 1, pos.z);
         merchant.position = pos;
         currentPath = 0;
