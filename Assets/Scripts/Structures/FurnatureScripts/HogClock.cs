102a8bade73944f53673a97099b6513610d570ce
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-04-12 12:49:10 -0400

    Hog clock

--- Assets/Scripts/Structures/FurnatureScripts/HogClock.cs
+++ Assets/Scripts/Structures/FurnatureScripts/HogClock.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class HogClock : FurnitureBehaviorScript
 {
     public Animator anim;
-    public AudioSource source;
+    public AudioSource source, gearSoundSource;
     public AudioClip hogSound, flySound;
+
+    public GameObject hog, fly;
+
+    public bool forceFly = false;
 
 
     void Start()
@@ -16,8 +20,38 @@ public class HogClock : FurnitureBehaviorScript
         
     }
 
+    [ContextMenu("Hour Passed")]
     public override void HourPassed()
     {
-        //
+        //if(TimeManager.Instance.currentHour == 6 || TimeManager.Instance.currentHour == 18)
+        //{
+            //
+        //}
+        StartCoroutine(PlayAnimation());
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        yield return new WaitForSeconds(Random.Range(0.2f, 0.8f));
+        bool isHog = true;
+        if(Random.Range(0, 10) >= 9 || forceFly) isHog = false;
+
+        hog.SetActive(isHog);
+        fly.SetActive(!isHog);
+
+        AudioClip soundToPlay = hogSound;
+        if(!isHog) soundToPlay = flySound;
+
+        anim.SetTrigger("Play");
+        gearSoundSource.Play();
+        yield return new WaitForSeconds(1.5f);
+        source.PlayOneShot(soundToPlay);
+        yield return new WaitForSeconds(0.7f);
+        source.Stop();
+        source.PlayOneShot(soundToPlay);
+        yield return new WaitForSeconds(0.7f);
+        source.Stop();
+        yield return new WaitForSeconds(0.5f);
+        gearSoundSource.Stop();
     }
 }
