a3821e7307bb722b53d25ec1eac85f9edc57f530
Abner-Wilhelm <abnerwilhelm@gmail.com>
2025-05-13 16:12:55 -0400

    StructureShopDisplay

---
+++ Assets/Scripts/Misc/StructureShopDisplay.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureShopDisplay : MonoBehaviour
+{
+    public Transform structureSpawnLocation;
+    GameObject spawnedStructure;
+
+    public void DisplayStructure(InventoryItemData itemData)
+    {
+        if (spawnedStructure != null) { LeaveShop(); }
+       PlaceableItem pItem = itemData as PlaceableItem;
+       if (pItem != null)
+        {
+            spawnedStructure = Instantiate(pItem.placedPrefab, structureSpawnLocation.position, structureSpawnLocation.rotation);
+            StructureBehaviorScript structureScript = spawnedStructure.GetComponent<StructureBehaviorScript>();
+            if (structureScript != null ) { structureScript.absentFromGrid = true; }
+            Destroy(structureScript);
+            AudioPoolManager.Instance.PlayClipAtPosition(pItem.placeSound, structureSpawnLocation.position);
+        }
+    }
+
+    public void LeaveShop()
+    {
+       Destroy(spawnedStructure);
+    }
+}
