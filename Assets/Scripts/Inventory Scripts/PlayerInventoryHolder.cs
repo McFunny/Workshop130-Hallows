aa394d3c11aff9f8fe3fd1c4f961c359568d6a22
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-06-02 15:45:44 -0400

    Update PlayerInventoryHolder.cs

--- Assets/Scripts/Inventory Scripts/PlayerInventoryHolder.cs
+++ Assets/Scripts/Inventory Scripts/PlayerInventoryHolder.cs
@@ -509,7 +509,7 @@ public class PlayerInventoryHolder : InventoryHolder
 
     public void SwitchHotBars(InputAction.CallbackContext obj)
     {
-        if(PlayerMovement.restrictMovementTokens > 0 || InputManager.isCharging || PauseScript.isPaused) return;
+        if(PlayerMovement.restrictMovementTokens > 0 || InputManager.isCharging || PauseScript.isPaused || PlayerMovement.accessingInventory) return;
 
         List<InventorySlot> currentPInventory = new List<InventorySlot>();
         List<InventorySlot> currentSInventoryRow1 = new List<InventorySlot>();
