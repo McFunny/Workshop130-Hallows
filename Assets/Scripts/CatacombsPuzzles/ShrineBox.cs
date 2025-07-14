ac19386a9d5d040d748aca817aee284cc8dcdc79
Abner-Wilhelm <abnerwilhelm@gmail.com>
2025-07-13 23:23:53 -0400

    Working on shrine box

---
+++ Assets/Scripts/CatacombsPuzzles/ShrineBox.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class ShrineBox : MonoBehaviour, IInteractable
+{
+    public UnityAction<IInteractable> OnInteractionComplete { get; set; }
+    public List<GameObject> highlight = new List<GameObject>();
+    List<Material> highlightMaterial = new List<Material>();
+    bool highlightEnabled;
+    public InventoryItemData wantedItem;
+    private int itemsDeposited;
+    public int itemsNeeded;
+    public bool isSolved = false;
+
+    public GameObject structureUI;
+    public TextMeshProUGUI structureUItext;
+    public SpriteRenderer itemWantedSprite;
+
+
+    public void EndInteraction()
+    {
+    }
+
+    public void Interact(PlayerInteraction interactor, out bool interactSuccessful)
+    {
+        interactSuccessful = false;
+    }
+
+    public void InteractWithItem(PlayerInteraction interactor, out bool interactSuccessful, InventoryItemData item)
+    {
+        interactSuccessful = false;
+        if (item == wantedItem)
+        {
+            HotbarDisplay.currentSlot.AssignedInventorySlot.RemoveFromStack(1);
+            interactor.playerInventoryHolder.UpdateInventory();
+            itemsDeposited++;
+            CheckToSeeIfSolved();
+            interactSuccessful = true;
+        }
+    }
+
+    private void CheckToSeeIfSolved()
+    {
+        if (itemsDeposited == itemsNeeded)
+        {
+            isSolved = true;
+            ToggleHighlight(false);
+            itemWantedSprite.color = Color.white;
+        }
+    }
+
+    public void ReturnFocalPoint(out Transform point)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ToggleHighlight(bool enable)
+    {
+        if (highlight.Count == 0) return;
+        if (highlightMaterial.Count == 0)
+        {
+            foreach (GameObject thing in highlight)
+                highlightMaterial.Add(highlight[0].GetComponentInChildren<MeshRenderer>().material);
+        }
+        if (enable && !highlightEnabled && !isSolved)
+        {
+            highlightEnabled = true;
+            foreach (GameObject thing in highlight) thing.SetActive(true);
+            StartCoroutine(HightlightFlash());
+            structureUI.SetActive(true);
+        }
+
+        if (!enable && highlightEnabled)
+        {
+            highlightEnabled = false;
+            foreach (GameObject thing in highlight) thing.SetActive(false);
+            structureUI.SetActive(false);
+        }
+    }
+
+    IEnumerator HightlightFlash()
+    {
+        float power = 1;
+        while (highlightEnabled)
+        {
+            
+            do
+            {
+                structureUItext.text = itemsDeposited + " / " + itemsNeeded;
+                yield return new WaitForSeconds(0.1f);
+                power -= 0.05f;
+                foreach (Material mat in highlightMaterial) mat.SetFloat("_Fresnel_Power", power);
+            }
+            while (power > 0.7f && highlightEnabled);
+            do
+            {
+                structureUItext.text = itemsDeposited + " / " + itemsNeeded;
+                yield return new WaitForSeconds(0.1f);
+                power += 0.05f;
+                foreach (Material mat in highlightMaterial) mat.SetFloat("_Fresnel_Power", power);
+            }
+            while (power < 1.9f && highlightEnabled);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemWantedSprite.sprite = wantedItem.icon;
+        itemWantedSprite.color = Color.black;
+    }
+
+}
