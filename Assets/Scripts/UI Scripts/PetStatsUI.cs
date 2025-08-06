3a2adab6aeffee313a9a92d710ee100385897894
SolidZan <zanman263@gmail.com>
2025-08-06 15:48:15 -0400

    I did not need that tag lol

--- Assets/Scripts/UI Scripts/PetStatsUI.cs
+++ Assets/Scripts/UI Scripts/PetStatsUI.cs
@@ -38,6 +38,7 @@ public class PetStatsUI : MonoBehaviour //Not finished yet lmao
     private Camera mainCam;
     public delegate void PetStatsShown();
     public event PetStatsShown OnPetStatsShown;
+    private int layerMask = (1 << 6 | 1 << 9);
     // Start is called before the first frame update
     void Start()
     {
@@ -79,49 +80,44 @@ public class PetStatsUI : MonoBehaviour //Not finished yet lmao
     {
         Vector3 fwd = mainCam.transform.TransformDirection(Vector3.forward);
         RaycastHit hit;
-        if (Physics.Raycast(mainCam.transform.position, fwd, out hit, reach, 1 << 6))
+        if (Physics.Raycast(mainCam.transform.position, fwd, out hit, reach, layerMask))
         {
-            IInteractable interactable = hit.collider.GetComponentInParent<IInteractable>();
-            if (interactable != null)
+            PetBehaviorScript pet = hit.collider.GetComponentInParent<PetBehaviorScript>();
+            CritterBehaviorScript critter = hit.collider.GetComponentInParent<CritterBehaviorScript>();
+            if (pet != null)
             {
-                if (interactable is PetBehaviorScript pet)
-                {
-                    isActive = true;
-                    creatureType = CreatureType.Pet;
-                    OnPetStatsShown?.Invoke();
-                    petNameText.text = pet.name;
-                    petTypeText.text = pet.petType.ToString();
-                    petHungerText.text = pet.hunger + "/" + pet.maxHunger;
-                    petThirstText.text = pet.thirst + "/" + pet.maxThirst;
-                    petHungerSlider.value = pet.hunger / pet.maxHunger;
-                    petThirstSlider.value = pet.thirst / pet.maxThirst;
-                    petFriendshipText.text = pet.friendshipLevel.ToString();
-                }
-                else if (interactable is CritterBehaviorScript critter)
-                {
-                    creatureType = CreatureType.Critter;
-                    OnPetStatsShown?.Invoke();
-                    critterNameText.text = critter.name;
-                    critterTypeText.text = critter.creatureData.name.ToString();
-                    critterHealthText.text = critter.health + "/" + critter.maxHealth;
-                    critterHungerText.text = critter.hunger + "/" + critter.maxHunger;
-                    critterThirstText.text = critter.thirst + "/" + critter.maxThirst;
-                    petHungerSlider.value = critter.health / critter.maxHealth;
-                    petHungerSlider.value = critter.hunger / critter.maxHunger;
-                    critterThirstSlider.value = critter.thirst / critter.maxThirst;
-                    critterFriendshipText.text = critter.friendshipLevel.ToString();
-                    isActive = true;
-                }
-                else
-                {
-                    isActive = false;
-                }
+                isActive = true;
+                creatureType = CreatureType.Pet;
+                OnPetStatsShown?.Invoke();
+                petNameText.text = pet.name;
+                petTypeText.text = pet.petType.ToString();
+                petHungerText.text = pet.hunger + "/" + pet.maxHunger;
+                petThirstText.text = pet.thirst + "/" + pet.maxThirst;
+                petHungerSlider.value = pet.hunger / pet.maxHunger;
+                petThirstSlider.value = pet.thirst / pet.maxThirst;
+                petFriendshipText.text = pet.friendshipLevel.ToString();
+            }
+            else if (critter != null)
+            {
+                isActive = true;
+                creatureType = CreatureType.Critter;
+                OnPetStatsShown?.Invoke();
+                critterNameText.text = critter.name;
+                critterHomeIcon.gameObject.SetActive(!critter.IsCritterHomeless());
+                critterTypeText.text = critter.creatureData.name.ToString();
+                critterHealthText.text = critter.health + "/" + critter.maxHealth;
+                critterHungerText.text = critter.hunger + "/" + critter.maxHunger;
+                critterThirstText.text = critter.thirst + "/" + critter.maxThirst;
+                petHungerSlider.value = critter.health / critter.maxHealth;
+                petHungerSlider.value = critter.hunger / critter.maxHunger;
+                critterThirstSlider.value = critter.thirst / critter.maxThirst;
+                critterFriendshipText.text = critter.friendshipLevel.ToString();
             }
             else
             {
                 isActive = false;
             }
-        }
+            }
         else
         {
             isActive = false;
