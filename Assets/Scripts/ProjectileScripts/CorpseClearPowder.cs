40ac302af3776c01a5c8b9ab525bf6c50659a67a
Cameron Rutkowski <moatsofgoats@gmail.com>
2025-08-14 14:37:06 -0400

    Corpse cloud

---
+++ Assets/Scripts/ProjectileScripts/CorpseClearPowder.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorpseClearPowder : MonoBehaviour
+{
+    public float duration = 15;
+    public CreatureObject walkerData;
+    public ParticleSystem effectParticles;
+
+    void Start()
+    {
+        StartCoroutine(LifeTime());
+    }
+
+    IEnumerator LifeTime()
+    {
+        float timeAlive = 0;
+        while(timeAlive < duration)
+        {
+            timeAlive++;
+            Collider[] hitEnemies = Physics.OverlapSphere(transform.position, 20f, 1 << 9);
+            foreach(Collider collider in hitEnemies)
+            {
+                var creature = collider.GetComponentInParent<CreatureBehaviorScript>();
+                if (creature != null && (creature.health <= 0 || creature.creatureData == walkerData))
+                {
+                    creature.TakeDamage(10);
+                    creature.PlayHitParticle(creature.transform.position);
+                }
+            }
+            yield return new WaitForSeconds(1);
+        }
+        effectParticles.Stop();
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+}
