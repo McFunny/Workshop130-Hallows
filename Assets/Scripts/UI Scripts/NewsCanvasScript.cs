7859be7c7efa3f38350b1e5242746cb0548c7c75
SolidZan <zanman263@gmail.com>
2025-08-28 00:37:51 -0400

    Changed canvas sort order at the main menu :)

--- Assets/Scripts/UI Scripts/NewsCanvasScript.cs
+++ Assets/Scripts/UI Scripts/NewsCanvasScript.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class NewsCanvasScript : MonoBehaviour
 {
-    [SerializeField] GameObject button;
+    [SerializeField] private GameObject button;
+    //[SerializeField] private TextMeshProUGUI descriptionObj;
+    //[TextArea(5,1)]
+    //[SerializeField] private string description;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //descriptionObj.text = description;
     }
 
     // Update is called once per frame
