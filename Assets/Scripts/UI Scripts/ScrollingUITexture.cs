9933766c1fadc7a0c84d9e400c8ef4b262e09136
SolidZan <zanman263@gmail.com>
2025-10-24 20:53:52 -0400

    Scrolling UI Texture stuff

---
+++ Assets/Scripts/UI Scripts/ScrollingUITexture.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScrollingUITexture : MonoBehaviour
+{
+    public float scrollSpeedX = 0.1f;
+    public float scrollSpeedY = 0.0f; // Set to 0 if only scrolling horizontally
+
+    private RawImage uiImage;
+
+    void Start()
+    {
+        uiImage = GetComponent<RawImage>();
+    }
+
+    private void OnEnable()
+    {
+        if (uiImage == null) return;
+        uiImage.uvRect = new Rect(0, 0, 1, 1);
+    }
+
+    void Update()
+    {
+        if (uiImage == null) return;
+        Rect offset = new Rect(uiImage.uvRect.x, uiImage.uvRect.y, uiImage.uvRect.width, uiImage.uvRect.height);
+        offset.x += scrollSpeedX * Time.deltaTime;
+        offset.y += scrollSpeedY * Time.deltaTime;
+        offset.x %= 1.0f;
+        offset.y %= 1.0f;
+        uiImage.uvRect = offset;
+    }
+}
