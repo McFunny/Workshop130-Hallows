dcc2effa5c202d484f01ee0bf32c216b13f5cf58
SolidZan <zanman263@gmail.com>
2025-11-18 17:42:13 -0500

    Frest fix

--- Assets/Scripts/UI Scripts/Codex3.cs
+++ Assets/Scripts/UI Scripts/Codex3.cs
@@ -50,8 +50,8 @@ public class Codex3 : MonoBehaviour
     [SerializeField] private AudioSource codexAudio;
     [SerializeField] private AudioClip codexOpenSound, codexCloseSound;
     [SerializeField] private Button[] categoryButtons;
-    [SerializeField] private GameObject[] containers;
-    [SerializeField] private GameObject[] secondaryContainers;
+    [SerializeField] private List<GameObject> containers = new List<GameObject>();
+    [SerializeField] private List<GameObject> secondaryContainers = new List<GameObject>();
     [SerializeField] private List<CodexPage> codexPages;
     [SerializeField] private TextMeshProUGUI categoryTitle;
     [SerializeField] private GameObject categoryContainer;
@@ -312,13 +312,14 @@ public class Codex3 : MonoBehaviour
     private void UpdateEntries()
     {
         if (TutorialList != null) ClearCodex(); // Clear the codex before updating entries. Checking the tutorial list should be sufficient.
-
+        print(containers.Count);
         // Run this on Start or when the codex is opened to update the entries
-        for (int i = 0; i < containers.Length; i++)
+        for (int i = 0; i < 8; i++) // CHANGE THIS IF WE EVER ADD MORE CATEGORIES
         {
             var Cat = TutorialEntries;
             var isQuest = false;
             var isCritter = false;
+            Debug.LogWarning(i);
             switch (i)
             {
                 case 0:
@@ -460,7 +461,7 @@ public class Codex3 : MonoBehaviour
 
                 //Critters
                 List<CritterBehaviorScript> critters = BarnManager.Instance.allCritters;
-                if (critters.Count == 0) return;
+                if (critters.Count == 0) continue;
 
                 for (int c = 0; c < critters.Count; c++)
                 {
@@ -757,7 +758,7 @@ public class Codex3 : MonoBehaviour
     private void ClearCodex()
     {
         // Clear all the entries in the codex
-        for (int i = 0; i < containers.Length; i++)
+        for (int i = 0; i < containers.Count; i++)
         {
             foreach (Transform child in containers[i].transform)
             {
@@ -816,7 +817,7 @@ public class Codex3 : MonoBehaviour
             else buttonLerps[i].lerpToStart = true;
         }
 
-        for (int i = 0; i < containers.Length; i++)
+        for (int i = 0; i < containers.Count; i++)
         {
             containers[i].SetActive(i == catInt); //i is true when i = categoryIndex. Did not know I could do this lol
             secondaryContainers[i].SetActive(i == catInt);
