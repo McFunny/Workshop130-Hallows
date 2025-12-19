b06602cc04985d9f11d8f94a3548b7da31578a30
SolidZan <zanman263@gmail.com>
2025-12-18 21:24:29 -0500

    Hide the version number 🔥🔥🔥

--- Assets/Scripts/Debug/HideUI.cs
+++ Assets/Scripts/Debug/HideUI.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class HideUI : MonoBehaviour
 {
     [SerializeField] CanvasGroup canvasGroup, tooltipCanvasGroup;
+    private CanvasGroup versionCanvas;
     [SerializeField] KeyCode mainKey, tooltipKey;
     public static bool hideUI = false;
     public System.Action onUIHidden, onUIShown;
+
+    void Awake()
+    {
+        versionCanvas = GameObject.Find("BuildCanvas").GetComponent<CanvasGroup>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -28,8 +34,19 @@ public class HideUI : MonoBehaviour
 
         if (tooltipKey != KeyCode.None && Input.GetKeyDown(tooltipKey) && tooltipCanvasGroup != null)
         {
-            if (tooltipCanvasGroup.alpha == 1) tooltipCanvasGroup.alpha = 0;
-            else tooltipCanvasGroup.alpha = 1;
+            if (tooltipCanvasGroup.alpha == 1)
+            {
+                if(versionCanvas == null) versionCanvas = GameObject.Find("BuildCanvas").GetComponent<CanvasGroup>();
+                tooltipCanvasGroup.alpha = 0;
+                versionCanvas.alpha = 0;
+            }
+            
+            else 
+            {
+                if(versionCanvas == null) versionCanvas = GameObject.Find("BuildCanvas").GetComponent<CanvasGroup>();
+                tooltipCanvasGroup.alpha = 1;
+                versionCanvas.alpha = 1;
+            }
         }
 
         if(canvasGroup.alpha == 0) hideUI = true;
