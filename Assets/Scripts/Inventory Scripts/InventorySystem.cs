19e8dbcee1bbe6aa448dc45c1ad1ecd2a10a5346
Cameron Rutkowski <moatsofgoats@gmail.com>
2026-01-15 16:49:26 -0500

    Fixed alex bug

--- Assets/Scripts/Inventory Scripts/InventorySystem.cs
+++ Assets/Scripts/Inventory Scripts/InventorySystem.cs
@@ -165,12 +165,23 @@ public class InventorySystem
 
     public void LoadFromSaveData(InventorySystemSaveData saveData, Database database) //Also call this for when we dynamically change inventory size
     {
-        inventorySlots.Clear();
         if(saveData.savedSlots == null)
         {
-            Debug.Log("No save data exists for this inventory"); //This means we need to load in empty slots
+            Debug.Log("No save data exists for this inventory"); //This means we need to load in empty slots, but we dont know which inventory this is, so we dont know how many slots to make
+
+            //Specifically for trinket system. Thank you Alex!//
+            InventorySlot slot = new InventorySlot();
+            inventorySlots.Add(slot); 
+
+            slot.acceptedItemType = InventorySlot.AcceptedItemType.Trinket;
+            TrinketInventoryData data = new TrinketInventoryData();
+
+            data.slot = slot;
+            TrinketInventoryHandler.Instance.trinkets.Add(data);
+            ////////////////////////////////////////////////////
             return;
         }
+        inventorySlots.Clear();
         foreach (var slotData in saveData.savedSlots)
         {
             if (slotData.itemID != -1)
