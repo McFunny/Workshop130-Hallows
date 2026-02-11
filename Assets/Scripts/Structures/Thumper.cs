45d79e9176bc903ee80ef2d33434532a7aded277
Cameron Rutkowski <moatsofgoats@gmail.com>
2026-02-10 19:06:33 -0500

    Fixed tink

--- Assets/Scripts/Structures/Thumper.cs
+++ Assets/Scripts/Structures/Thumper.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thumper : MonoBehaviour
+public class Thumper : StructureBehaviorScript
 {
+    public Animator anim;
+    public Renderer panel1, panel2, panel3;
+
+    public ParticleSystem smallPulse, mediumPulse, largePulse;
+
+    public float smallRange, mediumRange, largeRange;
+
+    public int charge = 0;
+    int maxCharge = 3;
+
     // Start is called before the first frame update
     void Start()
     {
