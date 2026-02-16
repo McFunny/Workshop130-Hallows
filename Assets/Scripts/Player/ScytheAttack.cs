c83fb0ae8c0daf9f6567778d775c122c90f04aa0
Cameron Rutkowski <moatsofgoats@gmail.com>
2026-02-15 20:47:16 -0500

    New craft n buffed scythe

--- Assets/Scripts/Player/ScytheAttack.cs
+++ Assets/Scripts/Player/ScytheAttack.cs
@@ -144,8 +144,8 @@ public class ScytheAttack : MonoBehaviour
             ParticlePoolManager.Instance.MoveAndPlayVFX(hitCreatures[i].GetComponentInChildren<Collider>().ClosestPoint(transform.position), ParticlePoolManager.Instance.hitEffect);
             hitCreatures[i].PlayHitParticle(hitCreatures[i].GetComponentInChildren<Collider>().ClosestPoint(transform.position));
 
-            float damage = 35;
-            if(upgradedSwing) damage += 10;
+            float damage = 40;
+            if(upgradedSwing) damage += 15;
             if(upgradedSwing && (hitCreatures[i].corpseType == CorpseParticleType.Red || hitCreatures[i].corpseType == CorpseParticleType.Corrupted) && hitCreatures[i].health > 0) 
             SummonBloodParticles(hitCreatures[i]);
 
