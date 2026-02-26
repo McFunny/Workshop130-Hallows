73151d72d7bb0f550011dbac94fd33fd06dbe4ae
Cameron Rutkowski <moatsofgoats@gmail.com>
2026-02-25 19:50:17 -0500

    themes

--- Assets/Scripts/ManagerScripts/TimeManager.cs
+++ Assets/Scripts/ManagerScripts/TimeManager.cs
@@ -315,7 +315,7 @@ public class TimeManager : MonoBehaviour
             if(currentHour < 8) targetHour = 7;
             else targetHour = currentHour + 5;
             
-            if(targetHour > 19) targetHour = 18;
+            if(targetHour > 19) targetHour = 19;
             //else targetHour = 19;
             while(currentHour != targetHour)
             {
